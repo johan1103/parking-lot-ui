ff825e998b1dc354b030d5bfcace8002697ad3a6
johan1103 <johan1103@naver.com>
2021-07-02 14:50:36 +0900

    컴포넌트 추가, slide_5

---
+++ parkinglot/src/components/Slide_4.vue
@@ -0,0 +1,46 @@
+<template>
+  
+          <div class="slide" id="4">
+              <div class="content fourth-content">
+                  <div class="container-fluid">
+                      <form id="contact" action="" method="post">
+                          <div class="row">
+                            <div class="col-md-12">
+                              <h2>Say Hello!</h2>
+                            </div>
+                            <div class="col-md-6">
+                              <fieldset>
+                                <input name="name" type="text" class="form-control" id="name" placeholder="Your name..." required="">
+                              </fieldset>
+                            </div>
+                            <div class="col-md-6">
+                              <fieldset>
+                                <input name="email" type="text" class="form-control" id="email" placeholder="Your email..." required="">
+                              </fieldset>
+                            </div>
+                            <div class="col-md-12">
+                              <fieldset>
+                                <textarea name="message" rows="6" class="form-control" id="message" placeholder="Your message..." required=""></textarea>
+                              </fieldset>
+                            </div>
+                            <div class="col-md-12">
+                              <fieldset>
+                                <button type="submit" id="form-submit" class="button">Send Now</button>
+                              </fieldset>
+                            </div>
+                          </div>
+                      </form>
+                  </div>
+              </div>
+          </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
